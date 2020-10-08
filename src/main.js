9f81575adbf9cb89dd0af05e08c10d0882f45207
Roma K <romankuchukov@gmail.com>
2020-10-07 21:03:02 -0400

    small edits

--- src/main.js
+++ src/main.js
@@ -6,16 +6,14 @@ const center = window.innerWidth / window.innerHeight;
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, center, 0.1, 1000);
 const renderer = new WebGLRenderer({ antialias: true });
-
 const geometry = new BoxGeometry(3, 3, 3);
 const material = new MeshBasicMaterial({ color: 0x0000ff });
 
 const cube = new Mesh(geometry, material);
 
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
-
 scene.add(cube);
 camera.position.z = 5;
 
