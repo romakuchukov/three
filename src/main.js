a166858c79d00743bb1effe1fc11a7864f209671
Roma K <romankuchukov@gmail.com>
2020-10-07 21:00:27 -0400

    exctracted init

--- src/main.js
+++ src/main.js
@@ -1,71 +1,41 @@
 import { BoxGeometry, Mesh, MeshBasicMaterial,  PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 
-let camera, scene, renderer, cube;
+const center = window.innerWidth / window.innerHeight;
 
-function init() {
-  // Init scene
-  scene = new Scene();
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, center, 0.1, 1000);
+const renderer = new WebGLRenderer({ antialias: true });
 
-  // Init camera (PerspectiveCamera)
-  camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+const geometry = new BoxGeometry(3, 3, 3);
+const material = new MeshBasicMaterial({ color: 0x0000ff });
 
-  // Init renderer
-  renderer = new WebGLRenderer({ antialias: true });
+const cube = new Mesh(geometry, material);
 
-  // Set size (whole window)
-  renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-  // Render to canvas element
-  document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-  // Init BoxGeometry object (rectangular cuboid)
-  const geometry = new BoxGeometry(3, 3, 3);
+scene.add(cube);
+camera.position.z = 5;
 
-  // Create material with color
-  const material = new MeshBasicMaterial({ color: 0x0000ff });
 
-  // Add texture -
-  // const texture = new THREE.TextureLoader().load('textures/crate.gif');
 
-  // Create material with texture
-  // const material = new THREE.MeshBasicMaterial({ map: texture });
-
-  // Create mesh with geo and material
-  cube = new Mesh(geometry, material);
-  // Add to scene
-  scene.add(cube);
-
-  // Position camera
-  camera.position.z = 5;
-}
-
-// Draw the scene every time the screen is refreshed
 function animate() {
   requestAnimationFrame(animate);
 
-  // Rotate cube (Change values to change speed)
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //cube.rotation.x += 0.01;
+  cube.rotation.y += -0.01;
 
   renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-  // Camera frustum aspect ratio
-  camera.aspect = window.innerWidth / window.innerHeight;
-  // After making changes to aspect
+  camera.aspect = center;
   camera.updateProjectionMatrix();
-  // Reset size
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 window.addEventListener('resize', onWindowResize, false);
 
-init();
 animate();
