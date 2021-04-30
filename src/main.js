09d50dcb37a8f365835c4d2abb89a6d8fd02fcbf
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-04-30 10:03:30 -0400

    isolated matrix and stopped rotation

--- src/main.js
+++ src/main.js
@@ -1,49 +1,49 @@
 import * as THREE from 'three';
 
+const mouse = new THREE.Vector2( 1, 1 );
+
 let camera, scene, renderer;
 
 let mesh;
-const amount = parseInt(window.location.search.substr(1)) || 10;
+const amount = 10;
 const count = Math.pow(amount, 3);
 const dummy = new THREE.Object3D();
+const middle = window.innerWidth / window.innerHeight;
 
-init();
+camera = new THREE.PerspectiveCamera(60, middle, 0.1, 100);
+camera.position.set(4, 4, 15);
+camera.lookAt(0, 0, 0);
+
+scene = new THREE.Scene();
+
+const loader = new THREE.BufferGeometryLoader();
+loader.load('dist/models/json/suzanne_buffergeometry.json', geometry => {
+
+  geometry.computeVertexNormals();
+  geometry.scale(0.5, 0.5, 0.5);
+
+  const material = new THREE.MeshNormalMaterial();
+  // check overdraw
+  // let material = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.1, transparent: true });
+
+  mesh = new THREE.InstancedMesh(geometry, material, count);
+  mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
+
+  scene.add(mesh);
+
+});
+
+renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+window.addEventListener('resize', onWindowResize);
 animate();
 
-function init() {
-
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
-  camera.position.set(amount * 0.9, amount * 0.9, amount * 0.9);
-  camera.lookAt(0, 0, 0);
-
-  scene = new THREE.Scene();
-
-  const loader = new THREE.BufferGeometryLoader();
-  loader.load('dist/models/json/suzanne_buffergeometry.json', function (geometry) {
-
-    geometry.computeVertexNormals();
-    geometry.scale(0.5, 0.5, 0.5);
-
-    const material = new THREE.MeshNormalMaterial();
-    // check overdraw
-    // let material = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.1, transparent: true });
-
-    mesh = new THREE.InstancedMesh(geometry, material, count);
-    mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
-    scene.add(mesh);
-
-  });
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  window.addEventListener('resize', onWindowResize);
-}
-
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.aspect = middle;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
@@ -55,21 +55,15 @@ function animate() {
 
 function render() {
   if (mesh) {
-
-    const time = Date.now() * 0.001;
-
-    mesh.rotation.x = Math.sin(time / 4);
-    mesh.rotation.y = Math.sin(time / 2);
+    //mesh.rotation.y += .01;
 
     let i = 0;
-    const offset = (amount - 1) / 2;
+    const offset = 5;
 
     for (let x = 0; x < amount; x ++) {
       for (let y = 0; y < amount; y ++) {
         for (let z = 0; z < amount; z ++) {
           dummy.position.set(offset - x, offset - y, offset - z);
-          dummy.rotation.y = (Math.sin(x / 4 + time) + Math.sin(y / 4 + time) + Math.sin(z / 4 + time));
-          dummy.rotation.z = dummy.rotation.y * 2;
           dummy.updateMatrix();
           mesh.setMatrixAt(i ++, dummy.matrix);
         }
